845e0a410b5932d23184c550ec9826db671c9275
Ryan-yang125 <1598094295@qq.com>
2020-12-22 19:50:38 +0800

    update

--- src/components/tree/tree.vue
+++ src/components/tree/tree.vue
@@ -21,7 +21,32 @@ export default {
       width: 460,
       height: 460,
       chartPadding: { top: 80, right: 80, bottom: 80, left: 80 },
-      data: [],
+      data: {
+        name: 'A1',
+        children: [
+          {
+            name: 'B1',
+            children: [
+              {
+                name: 'C1',
+                value: 100,
+              },
+              {
+                name: 'C2',
+                value: 300,
+              },
+              {
+                name: 'C3',
+                value: 200,
+              },
+            ],
+          },
+          {
+            name: 'B2',
+            value: 200,
+          },
+        ],
+      },
     };
   },
   // https://cn.vuejs.org/v2/api/#mounted
@@ -68,34 +93,7 @@ export default {
   methods: {
     initTree() {
       console.log(this.options);
-      // 读入data
-      const data = {
-        name: 'A1',
-        children: [
-          {
-            name: 'B1',
-            children: [
-              {
-                name: 'C1',
-                value: 100,
-              },
-              {
-                name: 'C2',
-                value: 300,
-              },
-              {
-                name: 'C3',
-                value: 200,
-              },
-            ],
-          },
-          {
-            name: 'B2',
-            value: 200,
-          },
-        ],
-      };
-      this.treeRoot = d3h.hierarchy(data);
+      this.treeRoot = d3h.hierarchy(this.data);
       // this.treeRoot = d3h.hierarchy(originData);
       // 指定图表的宽高
       this.width = 700 - this.chartPadding.right - this.chartPadding.left - 180;
@@ -113,62 +111,66 @@ export default {
         .attr('style', 'background: white')
         .attr('width', 700)
         .attr('height', 700);
-      // 添加g标签
-      this.g = this.svg
-        .append('g')
-        .attr('class', 'chart') // 图表部分
-        .attr(
-          'transform',
-          `translate(${this.chartPadding.left + 40}, ${this.chartPadding.top +
-            40})`
-        );
-      // 初始化树
-      this.treeLayout = d3h.tree();
-      this.treeLayout.size([400, 400]);
-      this.treeLayout(this.treeRoot);
-      // 	添加树节点groups
-      this.treeNodes = this.g
-        .append('g')
-        .attr('class', 'nodes')
-        .selectAll('node')
-        .data(this.treeRoot.descendants())
-        .enter()
-        .append('g')
-        .classed('node', true)
-        .attr('transform', (d) => `translate(${d.y},${d.x})`);
-      // 	绘制节点
-      this.treeNodes
-        .append('circle')
-        .classed('solid-node', true)
-        .attr('fill', (d) => (d.children ? '#555' : '#999'))
-        .attr('r', 2.5);
-      //	添加节点信息
-      this.treeNodes
-        .append('text')
-        .attr('dy', '0.31em')
-        .attr('x', (d) => (d.children ? -6 : 6))
-        .attr('text-anchor', (d) => (d.children ? 'end' : 'start'))
-        .text((d) => d.data.name)
-        .clone(true)
-        .lower()
-        .attr('stroke', 'white');
-      // 添加links
-      this.treeLinks = this.g
-        .append('g')
-        .attr('fill', 'none')
-        .attr('stroke', '#555')
-        .attr('stroke-opacity', 0.4)
-        .attr('stroke-width', 1.5)
-        .selectAll('path')
-        .data(this.treeRoot.links())
-        .join('path')
-        .attr(
-          'd',
-          d3
-            .linkHorizontal()
-            .x((d) => d.y)
-            .y((d) => d.x)
-        );
+
+      (() => {
+        // 添加g标签
+        this.g = this.svg
+          .append('g')
+          .attr('class', 'chart') // 图表部分
+          .attr(
+            'transform',
+            `translate(${this.chartPadding.left + 40}, ${this.chartPadding.top +
+              40})`
+          );
+        // 初始化树
+        this.treeLayout = d3h.tree();
+        this.treeLayout.size([400, 400]);
+        this.treeLayout(this.treeRoot);
+        // 	添加树节点groups
+        this.treeNodes = this.g
+          .append('g')
+          .attr('class', 'nodes')
+          .selectAll('node')
+          .data(this.treeRoot.descendants())
+          .enter()
+          .append('g')
+          .classed('node', true)
+          .attr('transform', (d) => `translate(${d.y},${d.x})`);
+        // 	绘制节点
+        this.treeNodes
+          .append('circle')
+          .classed('solid-node', true)
+          .attr('fill', (d) => (d.children ? '#555' : '#999'))
+          .attr('r', 2.5);
+        //	添加节点信息
+        this.treeNodes
+          .append('text')
+          .attr('dy', '0.31em')
+          .attr('x', (d) => (d.children ? -6 : 6))
+          .attr('text-anchor', (d) => (d.children ? 'end' : 'start'))
+          .text((d) => d.data.name)
+          .clone(true)
+          .lower()
+          .attr('stroke', 'white');
+        // 添加links
+        this.treeLinks = this.g
+          .append('g')
+          .attr('fill', 'none')
+          .attr('stroke', '#555')
+          .attr('stroke-opacity', 0.4)
+          .attr('stroke-width', 1.5)
+          .selectAll('path')
+          .data(this.treeRoot.links())
+          .join('path')
+          .attr(
+            'd',
+            d3
+              .linkHorizontal()
+              .x((d) => d.y)
+              .y((d) => d.x)
+          );
+      })();
+
       // 添加图表标题
       this.title = this.svg
         .append('g')
